6c9c2d5754d5b0e023c29b234e8b801ebd42856d
Daschmied <DouglasSchmieder@yahoo.com>
2016-03-29 10:01:01 -0400

    Updated Pokemon.h and bulbasaur.h
    Added a lot of details to them. Need Get functions still. Not sure what
    else.

--- pokemon_files/Pokemon.h
+++ pokemon_files/Pokemon.h
@@ -1,5 +1,8 @@
 //Include things. Not complete
 
+#include <ctime>
+#include <string>
+#include <iostream>
 #include "Moves.h"
 
 using namespace std;
@@ -10,12 +13,13 @@ HP = [(((Base+IV)x2+(root(EV)/4))xLevel)/100]+level+10
 Others = [((base+IV)x2+[root(EV)/4]xLevel)/100]+5
 IV range from 0-31. Generate average IV and give to every pokemon?
 Ignore EVs?
+Level up is 1/50 the base stat value. How do we do that?
 */
 
 class Pokemon{
 	public:
 		Pokemon();//Default constructor
-		//Get functions for all stats
+		//Get functions for all stats?
 		void setmhealth(int);//Set max health
 		void setchealth(int);//Set curr health
 		void setlevel(int);//Set level
@@ -26,10 +30,21 @@ class Pokemon{
 		void setspeed(int);//Set speed
 		void setxp(int);//Set current xp(should default to 0)
 		void setmxp(int);//Set max xp for levelup
+		void settype1(string);//Sets the type1
+		void settype2(string);//Sets the type2
+		void levelup();//The level up function for a pokemon
 		
 		
 		
 	private:
+		/*
+		int bhealth;//Base Health
+		int batt;//Base Attack
+		int bdef;//Base Defense
+		int bsatt;//Base Special Attack
+		int bsdef;//Base Special Defense
+		int bspeed;//Base Speed*///Was trying to set up a personal levelup
+		//Current levelup is simple random number generator for reasonable stat increase
 		int maxhealth;
 		int currhealth;
 		int level;
@@ -40,15 +55,82 @@ class Pokemon{
 		int speed;//Speed stat
 		int xp;//Current xp
 		int maxxp;//Max xp
+		string type1;//First type
+		string type2;//Second type/Set to Null if there is no second type
 };
 
 Pokemon::Pokemon() {
 	//Does a thing
-	
-	
+	srand(time(NULL));	
 }
 
 void Pokemon::setmhealth(int n) {
 	maxhealth = n;
 }
+
+void Pokemon::setchealth(int n) {
+	currhealth = n;
+}
+
+void Pokemon::setlevel(int n) {
+	level = n;
+}
+
+void Pokemon::setatt(int n) {
+	att = n;
+}
+
+void Pokemon::setdef(int n) {
+	def = n;
+}
+
+void Pokemon::setsatt(int n) {
+	satt = n;
+}
+
+void Pokemon::setsdef(int n) {
+	sdef = n;
+}
+
+void Pokemon::setspeed(int n) {
+	speed = n;
+}		
+
+void Pokemon::setxp(int n) {
+	xp = n;
+}
+
+void Pokemon::setmxp(int n) {
+	maxxp = n;
+}
+
+void Pokemon::settype1(string n) {
+	type1 = n;//Don't think these are right
+}
+
+void Pokemon::settype2(string n) {
+	type2 = n;//Don't think these are right
+}
+
+void Pokemon::levelup(int levelspeed) {
+	//With current algorithm, do not reset maxxp or curr xp
+	level++;
+	int chp, catt, cdef, csatt, csdef, cspeed;//Change stats
+	chp = (rand()%4)+2;
+	catt = (rand()%3)+1;
+	cdef = (rand()%3)+1;
+	csatt = (rand()%3)+1;
+	csdef = (rand()%3)+1;
+	cspeed = (rand()%3)+1;
+	maxhealth+=chp;
+	currhealth+=chp;
+	att+=catt;
+	def+=cdef;
+	satt+=csatt;
+	sdef+=csdef;
+	speed+=cspeed;
+	if (type == 3) {//Signifies a Medium Slow pokemon
+		maxxp = 6/5*pow(level,3)) - 15*level*level + 100*level - 140;
+	}
 		
+}		
--- pokemon_files/bulbasaur.h
+++ pokemon_files/bulbasaur.h
@@ -1,9 +1,10 @@
 //Bulbasaur Pokemon
 
 //Include things. Not complete
-#include <math.h>
+#include <cmath>
 #include "Moves.h"
 #include "Pokemon.h"
+
 
 using namespace std;
 
@@ -27,9 +28,14 @@ class Bulbasaur: public Pokemon {
 	public: 
 		Bulbasaur();
 		
-	private:
-		
-
+	private:		
+		int bhp;
+		int batt;
+		int bdef;
+		int bsatt;
+		int bsdef
+		int bspeed;
+		int levelspeed;
 
 };
 
@@ -39,24 +45,41 @@ Bulbasaur::Bulbasaur() {
 	//there's some really odd xp values for lvl 1 pokemon including fast having 1 xp 
 	//to lvl up.
 	int level=1;
-	int m;
+	levelspeed=3;//Medium Slow Pokemon
+	bhp = 45;
+	batt = 49;
+	bdef = 49;
+	bsatt = 65;
+	bsdef = 65;
+	bspeed = 45;
+	int m, hp, att, def, satt, sdef, speed;
+	//This xp determining algorithm determines MAX xp, not levelup xp
 	if(level > 1) {
-		m = (6/5*pow(level,3)) - 15*level*level + 100*level - 140
+		m = (6/5*pow(level,3)) - 15*level*level + 100*level - 140;
 	}
 	else {
 		m = 5;
 	}
+	hp = ((bhp*2)+(level)/100)+level+10;//Determine hp
+	att = (((batt*2)+level)/100)+5;//Determine att
+	def = (((bdef*2)+level)/100)+5;//Determine def
+	satt = (((bsatt*2)+level)/100)+5;//Determine satt
+	sdef = (((bsdef*2)+level)/100)+5;//Determine sdef
+	speed = (((bspeed*2)+level)/100)+5;//Determine speed
+	
 	//Calculated with no IV with all decimals rounded down
-	Pokemon::setmhealth(11);//Set max health
-	Pokemon::setchealth(11);//Set curr health
-	Pokemon::setlevel(1);//Set level
-	Pokemon::setatt(5);//Set attack
-	Pokemon::setdef(5);//Set defense
-	Pokemon::setsatt(6);//Set special attack
-	Pokemon::setsdef(6);//Set special defense
-	Pokemon::setspeed(5);//Set speed
+	Pokemon::setmhealth(hp);//Set max health
+	Pokemon::setchealth(hp);//Set curr health
+	Pokemon::setlevel(level);//Set level
+	Pokemon::setatt(att);//Set attack
+	Pokemon::setdef(def);//Set defense
+	Pokemon::setsatt(satt);//Set special attack
+	Pokemon::setsdef(sdef);//Set special defense
+	Pokemon::setspeed(speed);//Set speed
 	Pokemon::setxp(0);//Set current xp(should default to 0)
 	Pokemon::setmxp(m);//Set max xp for levelup
-
+	Pokemon::settype1("Grass");//Sets first type
+	Pokemon::settype2(Null);//Sets second type
 
 }
+
